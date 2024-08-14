ccf05a990a192b9e86d8e20e94b820c15a1e9075
gamer102345 <halimibrahim769@gmail.com>
2024-08-14 17:48:56 +0300

    start of email functionality

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 #will likely only use email
+
+import email
+import email.message 
+
+email_sender = 'ssec_pmp@gmail.com'
+email_passcode = 'mew0rk4ss3c'
+
+def email_send(body, header, email_receiver):
+    
+
+
+
+def email_read(email):
